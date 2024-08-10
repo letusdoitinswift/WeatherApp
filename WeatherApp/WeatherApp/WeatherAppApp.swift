51c1afb914720436170043baa91128e5d1cda5e7
Manish Gupta <manishgupta.swift@gmail.com>
2024-08-09 21:59:49 -0400

    Commit #1 - Adding initial project files, with .gitignore file. Removing xcuserdata from commit.

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Manish Gupta on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
